121a916d0ef08ca8ae6a5b7f17ac11a3c0dceb05
Kharbachzoubair <zoubairkharbach@gmail.com>
2025-03-01 23:16:50 +0000

    Saved local changes before pulling updates

--- client.c
+++ client.c
@@ -1,10 +1,42 @@
-#include <unistd.h>
-#include <signal.h>
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   client.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/27 00:39:51 by marvin            #+#    #+#             */
+/*   Updated: 2025/02/27 00:39:51 by marvin           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include <windows.h>
 #include <stdio.h>
 
+#define PIPE_NAME "\\\\.\\pipe\\MyPipe"
 
-int main()
+int main(int argc, char *argv[])
 {
-    char arv[] = "hello world hey ";
-    kill (15251, SIGUSR2);
+    HANDLE hPipe;
+    DWORD bytesWritten;
+
+    if (argc != 2)
+    {
+        printf("Usage: %s <Message>\n", argv[0]);
+        return 1;
+    }
+
+    hPipe = CreateFileA(
+        PIPE_NAME, GENERIC_WRITE, 0, NULL, OPEN_EXISTING, 0, NULL);
+
+    if (hPipe == INVALID_HANDLE_VALUE)
+    {
+        printf("Failed to connect to server.\n");
+        return 1;
+    }
+
+    WriteFile(hPipe, argv[1], strlen(argv[1]), &bytesWritten, NULL);
+    CloseHandle(hPipe);
+
+    printf("Message sent to server!\n");
+    return 0;
 }
--- server.c
+++ server.c
@@ -1,35 +1,53 @@
-#include <unistd.h>
-#include <signal.h>
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   server.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/27 00:39:19 by marvin            #+#    #+#             */
+/*   Updated: 2025/02/27 00:39:19 by marvin           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include <windows.h>
 #include <stdio.h>
 
-
-void    zoubir(int s)
-{
-    char c = 0;
-    int count = 0;
-    int bit;
-    if (s == SIGUSR1)
-        bit = 1;
-    else if (s == SIGUSR2)
-        bit = 0;
-
-    while (count < 8)
-    {
-        c
-        count++;
-    }
-}
-
-
+#define PIPE_NAME "\\\\.\\pipe\\MyPipe"
 
 int main()
 {
-    signal(SIGUSR1, zoubir);
-    signal(SIGUSR2, zoubir);
-    printf("server pid  =  %d\n", getpid());
-    while (1)
+    HANDLE hPipe;
+    char buffer[1024];
+    DWORD bytesRead;
+
+    printf("Server started. Waiting for client...\n");
+
+    hPipe = CreateNamedPipeA(
+        PIPE_NAME,              // Pipe name
+        PIPE_ACCESS_INBOUND,    // Read-only pipe
+        PIPE_TYPE_MESSAGE | PIPE_READMODE_MESSAGE | PIPE_WAIT, 
+        1,                      // Only one client
+        1024, 1024, 0, NULL);
+
+    if (hPipe == INVALID_HANDLE_VALUE)
     {
-        pause();
+        printf("Failed to create pipe.\n");
+        return 1;
     }
-    
+
+    if (ConnectNamedPipe(hPipe, NULL))
+    {
+        printf("Client connected!\n");
+        ReadFile(hPipe, buffer, sizeof(buffer), &bytesRead, NULL);
+        buffer[bytesRead] = '\0'; // Ensure null termination
+        printf("Received: %s\n", buffer);
+        DisconnectNamedPipe(hPipe);
+    }
+    else
+    {
+        printf("Failed to connect.\n");
+    }
+
+    CloseHandle(hPipe);
+    return 0;
 }
