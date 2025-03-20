170a88e19f6218f28721ed49befe2ac7e9e97ca9
Kharbachzoubair <zoubairkharbach@gmail.com>
2025-03-20 21:03:52 +0000

    done

--- client.c
+++ client.c
@@ -1,5 +1,5 @@
 /* ************************************************************************** */
-/*                                                                            */
+/*                                                                          */
 /*                                                        :::      ::::::::   */
 /*   client.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
@@ -13,7 +13,7 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <signal.h>
-#include "stdio.h"
+#include <stdio.h>
 
 unsigned long is_int(char *str)
 {
@@ -23,19 +23,19 @@ unsigned long is_int(char *str)
 
     if (str[i] == '+' || str[i] == '-')
     {
-        if (str[i] == '-')
-            sign = -1;
-        i++;
+    if (str[i] == '-')
+    sign = -1;
+    i++;
     }
     while (str[i])
     {
-        if (str[i] >= '0' && str[i] <= '9')
-            val = val * 10 + (str[i] - '0');
-        else
-            return (0);
-        if ((sign == 1 && val > 2147483647) || (sign == -1 && val > 2147483648))
-            return (0);
-        i++;
+    if (str[i] >= '0' && str[i] <= '9')
+    val = val * 10 + (str[i] - '0');
+    else
+    return (0);
+    if ((sign == 1 && val > 2147483647) || (sign == -1 && val > 2147483648))
+    return (0);
+    i++;
     }
     return (val * sign);
 }
@@ -45,12 +45,12 @@ void send_bit(pid_t server_pid, char c)
     int i = 7;
     while (i >= 0)
     {
-        if ((c >> i) & 1)
-            kill(server_pid, SIGUSR1);
-        else
-            kill(server_pid, SIGUSR2);
-        usleep(150);
-        i--;
+    if ((c >> i) & 1)
+    kill(server_pid, SIGUSR1);
+    else
+    kill(server_pid, SIGUSR2);
+    usleep(150);
+    i--;
     }
 }
 
@@ -58,8 +58,8 @@ void send_message(pid_t server_pid, char *message)
 {
     while (*message)
     {
-        send_bit(server_pid, *message);
-        message++;
+    send_bit(server_pid, *message);
+    message++;
     }
     send_bit(server_pid, '\0');
 }
@@ -68,17 +68,17 @@ int main(int argc, char *argv[])
 {
     if (argc != 3)
     {
-        printf("Usage: %s <Server PID> <Message>\n", argv[0]);
-        return (1);
+    printf("Usage: %s <Server PID> <Message>\n", argv[0]);
+    return (1);
     }
-    
+
     pid_t server_pid = is_int(argv[1]);
     if (server_pid <= 0)
     {
-        printf("SOS -- Invalid PID --\n");
-        return (1);
+    printf("SOS -- Invalid PID --\n");
+    return (1);
     }
-    
+
     send_message(server_pid, argv[2]);
 
 }
--- server.c
+++ server.c
@@ -6,47 +6,67 @@
 /*   By: zkharbac <zkharbac@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/27 00:39:19 by marvin            #+#    #+#             */
-/*   Updated: 2025/03/02 13:39:52 by zkharbac         ###   ########.fr       */
+/*   Updated: 2025/03/20 20:40:38 by zkharbac         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <signal.h>  
 #include <unistd.h>  
 #include <stdio.h>   
+#include <stdlib.h>
+#include <signal.h>
 
-
-void    zoubir(int signal)
+void signal_handler(int signal, siginfo_t *info, void *context)
 {
-    static char c = 0;
-    static int count = 0;
-
-    
-    int bit = (signal == SIGUSR1) ? 1 : 0;
-
+	static char c = 0;
+	static int count = 0;
+	static pid_t pid = -1;
    
-    c = (c << 1) | bit;
-    count++;
 
-   
-    if (count == 8)
-    {
-        write(1, &c, 1); 
-        count = 0;
-        c = 0;
-    }
-}
+	(void)context;
+	if (pid != info->si_pid)
+	{
+		count = 0;
+		c = 0;
+		pid = info->si_pid;
+	}
+	
+	int bit = (signal == SIGUSR1) ? 1 : 0;
+
+	c = (c << 1) | bit;
+	count++;
+
+	if (count == 8) 
+	{
+	if (c == '\0') 
+	write(1, "\n", 1);
+	else 
+	write(1, &c, 1);
+	
+	c = 0;
+	count = 0;
+	}
+
+	}
 
 
 int main()
 {
-    printf("Server PID: %d\n", getpid());
+	struct sigaction sa;
+	sa.sa_sigaction = signal_handler;
+	sa.sa_flags = SA_SIGINFO;
 
-    // Listen for signals
-    signal(SIGUSR1, zoubir);
-    signal(SIGUSR2, zoubir);
+	sigemptyset(&sa.sa_mask);
 
-    // Keep running
-    while (1)
-        pause(); // Wait for signals
+	printf("Server PID: %d\n", getpid());
+
+	if (sigaction(SIGUSR1, &sa, NULL) == -1 || sigaction(SIGUSR2, &sa, NULL) == -1)
+	{
+	perror("sigaction failed");
+	return EXIT_FAILURE;
+	}
+
+	while (1)
+	pause();
+
+	return 0;
 }
-
